5b0682fb60dc5b4a7a42208f64fb9fd84b0bfd56
AdilGumarov <adilgumarov@gmail.com>
2023-05-14 20:48:27 +0600

    create project

---
+++ Geagraphic Atlas/Geagraphic Atlas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Geagraphic Atlas
+//
+//  Created by Adil Gumarov on 14.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
